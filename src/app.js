9064d6aa09a39143c319af168bcacf7fcb436d9a
Emeric Francisod <emeric.francisod+github@gmail.com>
2022-06-25 11:25:30 +0200

    Create User mongoose schema
    Connect to MongoDB database.
    
    Create User schema to represent the user.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 import express from 'express';
 import { fileURLToPath } from 'node:url';
 import path from 'node:path';
+import mongoose from 'mongoose';
 
 const app = express();
+
+mongoose
+    .connect('mongodb+srv://HotTakesApi:NvCfLecSOV9mByFz@hottakes.8l0ie.mongodb.net/?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log('MongoDB connection successful.'))
+    .catch(() => {
+        console.error('MongoDB connection failed.');
+    });
 
 /* Store the root folder absolute path in the app.
  * Since we are using ES modules, __dirname is unavailable.
