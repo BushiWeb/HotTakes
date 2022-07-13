f41c757a2504ff3a96124d93403488c2a02e0d94
Emeric Francisod <emeric.francisod+github@gmail.com>
2022-07-13 09:03:27 +0200

    Create checkOwnership middleware
    This middleware checks that the user owns the sauce is trying to access.
    Checking is done with the data from the database.

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,8 +1,10 @@
 import jsonWebToken from 'jsonwebtoken';
+import Sauce from '../models/Sauce.js';
 
 /**
  * Middleware, checks that the user is authenticated while making the request, by checking that the authentication token is valid.
- * Stores the userId in the request object.
+ * Stores the userId in the request object and go to the next middleware if the JsonWebToken and the userId are valid.
+ * Go to the next error middleware if there is a problem.
  * @param {Express.Request} req - Express request object.
  * @param {Express.Response} res - Express response object.
  * @param next - Next middleware to execute.
@@ -17,6 +19,31 @@ export const checkAuthentication = (req, res, next) => {
         req.auth = { userId: decodedToken.userId };
         next();
     } catch (error) {
-        return next({ message: 'Invalid request, you must be authenticated.', status: 401, error });
+        return next({ message: 'Invalid request, you must be authenticated', status: 401, error });
+    }
+};
+
+/**
+ * Middleware, checks that the user owns the request subject.
+ * Fetches the subject from the database and compares it's userId with the request's userId.
+ * Go to the next middleware if the user owns the subject, and to the next error middleware otherwise.
+ * @param {Express.Request} req - Express request object.
+ * @param {Express.Response} res - Express response object.
+ * @param next - Next middleware to execute.
+ */
+export const checkOwnership = async (req, res, next) => {
+    try {
+        const sauce = await Sauce.findById(req.params.id).exec();
+        if (!sauce) {
+            throw { message: "The ressource you're requesting doesn't exist", status: 404 };
+        }
+
+        if (sauce.userId !== req.auth.userId) {
+            throw { message: "Invalid request, you dont't have the right to access this ressource", status: 403 };
+        }
+
+        next();
+    } catch (error) {
+        return next(error);
     }
 };
--- test/middlewares/authentication.test.js
+++ test/middlewares/authentication.test.js
@@ -1,10 +1,14 @@
-import { checkAuthentication } from '../../src/middlewares/authentication.js';
+import { checkAuthentication, checkOwnership } from '../../src/middlewares/authentication.js';
 import { mockResponse, mockRequest, mockNext } from '../mocks/express-mocks.js';
 import jsonWebToken from 'jsonwebtoken';
 import { JsonWebTokenError } from 'jsonwebtoken';
+import { Query, Model } from 'mongoose';
 
 const mockJWTVerify = jest.spyOn(jsonWebToken, 'verify');
 const authorizationHeader = 'Bearer 123312';
+
+const mockFindById = jest.spyOn(Model, 'findById').mockImplementation(() => new Query());
+const mockExec = jest.spyOn(Query.prototype, 'exec');
 
 const request = mockRequest({}, { authorization: authorizationHeader });
 const response = mockResponse();
@@ -15,6 +19,8 @@ beforeEach(() => {
     response.status.mockClear();
     response.json.mockClear();
     next.mockClear();
+    mockFindById.mockClear();
+    mockExec.mockClear();
     delete request.auth;
 });
 
@@ -72,5 +78,45 @@ describe('checkAuthentication test suite', () => {
         expect(next.mock.calls[0][0]).toHaveProperty('message');
         expect(next.mock.calls[0][0]).toHaveProperty('status', 401);
         expect(next.mock.calls[0][0]).toHaveProperty('error');
+        request.headers.authorization = authorizationHeader;
+    });
+});
+
+describe('checkOwnership test suite', () => {
+    test('Calls next if the user id is right', async () => {
+        request.auth = { userId: '123' };
+        request.params = { id: 'sauceId' };
+        const sauce = { name: 'Tabasco', userId: '123' };
+        mockExec.mockResolvedValue(sauce);
+
+        await checkOwnership(request, response, next);
+
+        expect(next).toHaveBeenCalled();
+        expect(next.mock.calls[0].length).toBe(0);
+    });
+
+    test('Calls the next function with an error containing the status 403 if the user id is wrong', async () => {
+        request.auth = { userId: '456' };
+        request.params = { id: 'sauceId' };
+        const sauce = { name: 'Tabasco', userId: '123' };
+        mockExec.mockResolvedValue(sauce);
+
+        await checkOwnership(request, response, next);
+
+        expect(next).toHaveBeenCalled();
+        expect(next.mock.calls[0][0]).toHaveProperty('message');
+        expect(next.mock.calls[0][0]).toHaveProperty('status', 403);
+    });
+
+    test("Calls the next function with an error containing the status 404 if the element doesn't exist", async () => {
+        request.auth = { userId: '456' };
+        request.params = { id: 'sauceId' };
+        mockExec.mockResolvedValue(null);
+
+        await checkOwnership(request, response, next);
+
+        expect(next).toHaveBeenCalled();
+        expect(next.mock.calls[0][0]).toHaveProperty('message');
+        expect(next.mock.calls[0][0]).toHaveProperty('status', 404);
     });
 });
