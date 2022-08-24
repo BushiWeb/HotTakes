78a07cb82ca71d0790098a012b4f787b92c4e9a6
Emeric Francisod <emeric.francisod+github@gmail.com>
2022-08-24 15:42:07 +0200

    Create middleware to validate payload
    Uses schema validation.

--- src/middlewares/field-validation.js
+++ src/middlewares/field-validation.js
@@ -2,6 +2,7 @@ import { validationResult } from 'express-validator';
 import { errorFormatter } from '../utils/utils-validation.js';
 import UserInputValidationError from '../errors/UserInputValidationError.js';
 import { createDebugNamespace } from '../logger/logger.js';
+import ajv from '../schemas/json-validator.js';
 
 const validationDebug = createDebugNamespace('hottakes:middleware:validation');
 
@@ -23,4 +24,37 @@ export const validateFields = (req, res, next) => {
         error = error.formatWith(errorFormatter).array();
         return next(new UserInputValidationError(error));
     }
+};
+
+/**
+ * Payload validation middleware.
+ * Returns a middleware that validates the body against the specified JSON schema. If there is an error, call the next error middleware with a UserInputValidationError.
+ * @param {string} schemaName - Name of the schema to uszer for the validation.
+ */
+export const validatePayload = (schemaName) => {
+    validationDebug(`Create the payload validator for the ${schemaName} schema`);
+    return (req, res, next) => {
+        validationDebug('Validation middleware execution: payload validation');
+        const validate = ajv.getSchema(schemaName);
+
+        // Throws an error if the schema doesn't exist
+        if (!validate) {
+            return next(new Error(`The JSON schema ${schemaName} doesn't exist`));
+        }
+
+        // Calls the next middleware if the body is valid
+        if (validate(req.body)) {
+            return next();
+        }
+
+        // Generates a UserInputValidationMiddleware if the body is invalid
+        let errors = [];
+        for (const error of validate.errors) {
+            errors.push({
+                property: error.instancePath,
+                message: error.message,
+            });
+        }
+        return next(new UserInputValidationError(errors));
+    };
 };
--- test/middlewares/field-validation.test.js
+++ test/middlewares/field-validation.test.js
@@ -1,10 +1,27 @@
-import { validateFields } from '../../src/middlewares/field-validation';
+import { validateFields, validatePayload } from '../../src/middlewares/field-validation';
 import { mockResponse, mockRequest, mockNext } from '../mocks/express-mocks.js';
 import { Result } from 'express-validator';
 import { mockResultError } from '../mocks/express-validator-mocks.js';
 import UserInputValidationError from '../../src/errors/UserInputValidationError';
+import ajv from '../../src/schemas/json-validator.js';
 
 const mockValidationResultThrow = jest.spyOn(Result.prototype, 'throw');
+const testSchemaName = 'test';
+ajv.addSchema(
+    {
+        type: 'object',
+        properties: {
+            p1: {
+                type: 'string',
+            },
+            p2: {
+                type: 'integer',
+            },
+        },
+        required: ['p1', 'p2'],
+    },
+    testSchemaName
+);
 
 const request = mockRequest({
     email: 'test@email.com',
@@ -18,6 +35,13 @@ beforeEach(() => {
     response.status.mockClear();
     response.json.mockClear();
     next.mockClear();
+    request.body = {};
+});
+
+afterAll(() => {
+    response.status.mockRestore();
+    response.json.mockRestore();
+    next.mockRestore();
 });
 
 describe('validateFields test suite', () => {
@@ -39,6 +63,36 @@ describe('validateFields test suite', () => {
         });
 
         validateFields(request, response, next);
+
+        expect(next).toHaveBeenCalled();
+        expect(next.mock.calls[0][0]).toBeInstanceOf(UserInputValidationError);
+    });
+});
+
+describe.only('validatePayload middleware test suite', () => {
+    test('Calls the next function if the body is valid', () => {
+        request.body = {
+            p1: 'test',
+            p2: 3,
+        };
+        validatePayload(testSchemaName)(request, response, next);
+
+        expect(next).toHaveBeenCalled();
+        expect(next.mock.calls[0].length).toBe(0);
+    });
+
+    test("Calls the next function with an error if the schema doesn't exist", () => {
+        validatePayload('undefinedSchema')(request, response, next);
+
+        expect(next).toHaveBeenCalled();
+        expect(next.mock.calls[0][0]).toBeInstanceOf(Error);
+    });
+
+    test('Calls the next function with a UserInputValidationError if the body is not valid', () => {
+        request.body = {
+            p2: '3',
+        };
+        validatePayload(testSchemaName)(request, response, next);
 
         expect(next).toHaveBeenCalled();
         expect(next.mock.calls[0][0]).toBeInstanceOf(UserInputValidationError);
